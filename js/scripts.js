5d60436d43a784d617b42257fd62345866be8ce7
Andrew <expandrew@gmail.com>
2017-03-06 17:47:37 -0800

    finish writing all questions and final results.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   $("#userName form").submit(function(event){
     var userName = ("userName");
-    $("#userName form").fadeOut();
-    $("#question1").fadeIn();
+    $("#userName form").hide();
+    $("#question1").show();
 
     event.preventDefault();
   })
