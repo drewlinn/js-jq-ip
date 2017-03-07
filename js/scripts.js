7fd582548dc9143f6bd222586f68348f08399dd7
Andrew <expandrew@gmail.com>
2017-03-07 00:42:19 -0800

    correct syntax errors in scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 //business logic
-var destiNation =
 var question1 = $("input:radio[name=question1]:checked").val();
 var question2A = $("input:radio[name=question2A]:checked").val();
 var question2B = $("input:radio[name=question2B]:checked").val();
@@ -20,114 +19,114 @@ var question4H = $("input:radio[name=question4H]:checked").val();
 $(document).ready(function()  {
   $("form#userName").submit(function(event)  {
     var userName = $("input#userName").val();
+    event.preventDefault();
     $("#userName").text(userName)
+    $("form#userName").hide();
+    $(".question1").show();
     if (question1 === "A")  {
       $(".question1").hide();
       $(".question2A").show();
-    } else if (question1 === "B")
+    } else if (question1 === "B") {
       $(".question1").hide();
       $(".question2B").show();
-    }
+    };
     if (question2A === "A")  {
       $(".question2A").hide();
       $(".question3A").show();
-    } else if (question2A === "B")
+    } else if (question2A === "B")  {
       $(".question2A").hide();
       $(".question3B").show();
-    }
+    };
     if (question2B === "A")  {
       $(".question2B").hide();
       $(".question3C").show();
-    } else if (question2B === "B")
+    } else if (question2B === "B")  {
       $(".question2B").hide();
       $(".question3D").show();
-    }
+    };
     if (question3A === "A")  {
       $(".question3A").hide();
       $(".question4A").show();
-    } else if (question3A === "B")
+    } else if (question3A === "B")  {
       $(".question3A").hide();
       $(".question4B").show();
-    }
+    };
     if (question3B === "A")  {
       $(".question3B").hide();
       $(".question4C").show();
-    } else if (question3B === "B")
+    } else if (question3B === "B")  {
       $(".question3B").hide();
       $(".question4D").show();
-    }
+    };
     if (question3C === "A")  {
       $(".question3C").hide();
       $(".question4E").show();
-    } else if (question3C === "B")
+    } else if (question3C === "B")  {
       $(".question3C").hide();
       $(".question4F").show();
-    }
+    };
     if (question3D === "A")  {
       $(".question3D").hide();
       $(".question4G").show();
-    } else if (question3D === "B")
+    } else if (question3D === "B")  {
       $(".question3D").hide();
       $(".question4H").show();
-    }
+    };
     if (question4A === "A")  {
       $(".question4A").hide();
       $(".London").show();
-    } else if (question4A === "B")
+    } else if (question4A === "B")  {
       $(".question4A").hide();
       $(".NewYork").show();
-    }
+    };
     if (question4B === "A")  {
       $(".question4B").hide();
       $(".Rome").show();
-    } else if (question4B === "B")
+    } else if (question4B === "B")  {
       $(".question4B").hide();
       $(".Istanbul").show();
-    }
+    };
     if (question4C === "A")  {
       $(".question4C").hide();
       $(".Amsterdam").show();
-    } else if (question4C === "B")
+    } else if (question4C === "B")  {
       $(".question4C").hide();
       $(".Paris").show();
-    }
+    };
     if (question4D === "A")  {
       $(".question4D").hide();
       $(".Barcelona").show();
-    } else if (question4D === "B")
+    } else if (question4D === "B")  {
       $(".question4D").hide();
       $(".Milan").show();
-    }
+    };
     if (question4E === "A")  {
       $(".question4E").hide();
       $(".Bangkok").show();
-    } else if (question4E === "B")
+    } else if (question4E === "B")  {
       $(".question4E").hide();
       $(".HongKong").show();
-    }
+    };
     if (question4F === "A")  {
       $(".question4F").hide();
       $(".KualaLumpur").show();
-    } else if (question4F === "B")
+    } else if (question4F === "B")  {
       $(".question4F").hide();
       $(".Singapore").show();
-    }
+    };
     if (question4G === "A")  {
       $(".question4G").hide();
       $(".Dubai").show();
-    } else if (question4G === "B")
+    } else if (question4G === "B")  {
       $(".question4G").hide();
       $(".Tokyo").show();
-    }
+    };
     if (question4H === "A")  {
       $(".question4H").hide();
       $(".Seoul").show();
-    } else if (question4H === "B")
+    } else if (question4H === "B")  {
       $(".question4H").hide();
       $(".Taipei").show();
-    }
-
-
-    event.preventDefault();
+    };
   });
 });
