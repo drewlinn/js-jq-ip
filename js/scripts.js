63540e3b64064079fd919e8fc2fc20f6a00fd2ca
Andrew <expandrew@gmail.com>
2017-03-09 19:53:44 -0800

    fix bugs where styles.css was not properly applying styles to the page.

--- js/scripts.js
+++ js/scripts.js
@@ -11,16 +11,16 @@ $(document).ready(function()  {
     var question3 = $("input:radio[name=question3]:checked").val();
     var question4 = $("input:radio[name=question4]:checked").val();
     var question5 = $("input:radio[name=question5]:checked").val();
-      $("#quiz").hide();
-      if (question1 === "A" && question2 === "A" && question3 === "A" && question4 === "A" && question5 === "A") {
-      $("#results#NewYork").show();
-    } else if (question1 === "B" && question2 === "B" && question3 === "B" && question4 === "B" && question5 === "B") {
-      $("#results#Chicago").show();
-    } else if (question1 === "C" && question2 === "C" && question3 === "C" && question4 === "C" && question5 === "C") {
-      $("#results#Charleston").show();
-    }  else if (question1 === "D" && question2 === "D" && question3 === "D" && question4 === "D" && question5 === "D") {
-      $("#results#LasVegas").show();
-    }
+    $("#quiz").hide();
+    if (question1 === "A" && question2 === "A" && question3 === "A" && question4 === "A" && question5 === "A") {
+    $("#NewYork").show();
+  } else if (question1 === "B" && question2 === "B" && question3 === "B" && question4 === "B" && question5 === "B") {
+    $("#Chicago").show();
+  } else if (question1 === "C" && question2 === "C" && question3 === "C" && question4 === "C" && question5 === "C") {
+    $("#Charleston").show();
+  }  else if (question1 === "D" && question2 === "D" && question3 === "D" && question4 === "D" && question5 === "D") {
+    $("#LasVegas").show();
+  }
 
   });
 });
